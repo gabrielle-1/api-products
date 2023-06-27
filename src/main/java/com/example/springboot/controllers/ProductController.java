1bb444934838a1e9279f13efc03991894e02d0c3
gabrielle-1 <gabriellerodriguesmd@gmail.com>
2023-06-27 14:57:33 -0300

    Refatoring a ProductService and ProductController

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -4,18 +4,13 @@ import com.example.springboot.dtos.ProductRecordDto;
 import com.example.springboot.models.ProductModel;
 import com.example.springboot.services.ProductService;
 import jakarta.validation.Valid;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class ProductController {
