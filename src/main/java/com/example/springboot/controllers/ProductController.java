cc7f9d7a16273bf8ef53403080a70e5a626d0445
gabrielle-1 <gabriellerodriguesmd@gmail.com>
2023-07-05 11:31:26 -0300

    Added a documentation API with Swagger

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -4,10 +4,12 @@ import com.example.springboot.dtos.ProductRecordDto;
 import com.example.springboot.models.ProductModel;
 import com.example.springboot.services.ProductService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@Tag(name = "Product API", description = "API for product management")
 @RestController
 public class ProductController {
 
@@ -28,32 +31,51 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @Operation(summary = "Create a new product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Product created",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ProductModel.class)))
+    })
     @PostMapping("/products")
     public ResponseEntity<ProductRecordDto> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
         var productSaved = this.productService.saveProduct(productRecordDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(productSaved);
     }
 
-    @Operation(summary = "Obter usuário por ID")
+    @Operation(summary = "Retrieve all products")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Usuário encontrado",
+            @ApiResponse(responseCode = "200", description = "Recovered products",
                     content = @Content(mediaType = "application/json",
-                            schema = @Schema(implementation = ProductModel.class))),
-            @ApiResponse(responseCode = "404", description = "Usuário não encontrado")
+                            schema = @Schema(implementation = ProductModel.class)))
     })
     @GetMapping("/products")
-    public ResponseEntity<List<ProductModel>> getAllProducts(Pageable pageable) {
+    public ResponseEntity<List<ProductModel>> getAllProducts(@Parameter(hidden = true) Pageable pageable) {
         List<ProductModel> products = this.productService.getAllProducts(pageable).getContent();
         return ResponseEntity.status(HttpStatus.OK).body(products);
     }
 
+    @Operation(summary = "Get product by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product found",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ProductModel.class))),
+            @ApiResponse(responseCode = "404", description = "Product not found")
+    })
     @GetMapping("/products/{id}")
-    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") @NotNull @Positive UUID id, @NotNull @Positive Pageable pageable) {
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") @NotNull @Positive UUID id, @Parameter(hidden = true) @NotNull @Positive Pageable pageable) {
         Object product = this.productService.getOneProduct(id, pageable);
         if (product == null) return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
         return ResponseEntity.status(HttpStatus.OK).body(product);
     }
 
+    @Operation(summary = "Update product by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product updated",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ProductModel.class))),
+            @ApiResponse(responseCode = "404", description = "Product not found")
+    })
     @PutMapping("/products/{id}")
     public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") @NotNull @Positive UUID id,
                                                 @RequestBody @Valid ProductRecordDto productRecordDto) {
@@ -65,6 +87,11 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(product);
     }
 
+    @Operation(summary = "Delete product by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product deleted"),
+            @ApiResponse(responseCode = "404", description = "Product not found")
+    })
     @DeleteMapping("/products/{id}")
     public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") @NotNull @Positive UUID id) {
         var product = this.productService.deleteProduct(id);
